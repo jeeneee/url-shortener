8c426e90b5c02eb0690a6da2e50a1f1778125bb8
jeeneee <woojin7124@gmail.com>
2021-11-02 22:57:18 +0900

    [Feat] Cors 설정 및 정책 변경

--- src/main/java/com/jeeneee/urlshortener/controller/UrlController.java
+++ src/main/java/com/jeeneee/urlshortener/controller/UrlController.java
@@ -8,7 +8,9 @@ import java.net.URI;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,16 +30,21 @@ public class UrlController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> createShortUrl(@Valid @RequestBody UrlRequest request) {
+    public ResponseEntity<String> createShortUrl(@Valid @RequestBody UrlRequest request) {
         Url url = urlService.saveIfAbsent(request);
-        return ResponseEntity.created(URI.create(baseUrl + url.getShortUrl())).build();
+        String location = baseUrl + url.getShortUrl();
+        return ResponseEntity.created(URI.create(location)).body(location);
     }
 
     @GetMapping("/{shortUrl}")
     public void redirect(@PathVariable String shortUrl, HttpServletResponse response)
         throws IOException {
         Url url = urlService.findByShortUrl(shortUrl);
-        String redirectUrl = url == null ? baseUrl : url.getLongUrl();
-        response.sendRedirect(redirectUrl);
+        response.sendRedirect(url.getLongUrl());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> exceptionHandler(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
