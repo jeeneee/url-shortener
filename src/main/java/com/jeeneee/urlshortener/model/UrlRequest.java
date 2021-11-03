39de9dd5e5d0f8d033f8f551c46464e5afce4413
jeeneee <woojin7124@gmail.com>
2021-11-03 20:11:17 +0900

    [Feat] URL 패턴 수정

--- src/main/java/com/jeeneee/urlshortener/model/UrlRequest.java
+++ src/main/java/com/jeeneee/urlshortener/model/UrlRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UrlRequest {
 
-    @Pattern(regexp = "[(https?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&//=]*)")
+    @Pattern(regexp = "[https?://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&//=]*)")
     private String longUrl;
 
     public UrlRequest(String longUrl) {
